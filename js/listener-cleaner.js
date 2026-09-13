f508b4a6e4d9b204f14b1632731a81d653d1d3a2
Jawi <98896579+Jawi40@users.noreply.github.com>
2026-09-12 20:35:57 -0400

    Update listener-cleaner.js

--- js/listener-cleaner.js
+++ js/listener-cleaner.js
@@ -10,7 +10,7 @@ try {
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
     }),
-    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`
   });
 
   console.log("Firebase initialized successfully.");
